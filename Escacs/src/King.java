4bdf023a5f93e8d301beda9a74c547850c29ef31
Jordi Albert <63541019+jarg1023@users.noreply.github.com>
2020-11-15 19:55:17 +0100

    Create pieces files

---
+++ Escacs/src/King.java
@@ -0,0 +1,13 @@
+public class King extends Piece{
+    
+    public King (int X1, int Y1, int X2, int Y2, String turn, String[][] board, String pattern) {
+        super(X1, Y1, X2, Y2, turn, board, pattern);
+    }
+
+    @Override
+    public boolean isValidMove() {
+        boolean valid = false;
+
+        return valid;
+    }
+}
---
+++ Escacs/src/Queen.java
@@ -0,0 +1,14 @@
+public class Queen extends Piece {
+
+    public Queen (int X1, int Y1, int X2, int Y2, String turn, String[][] board, String pattern) {
+        super(X1, Y1, X2, Y2, turn, board, pattern);
+    }
+
+    @Override
+    public boolean isValidMove() {
+        boolean valid = false;
+        
+
+        return valid;
+    }
+}
